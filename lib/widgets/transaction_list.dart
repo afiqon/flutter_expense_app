9c23f1c0f713727dcab96810e50b124f99b5b6dc
Afiq <afiqon@gmail.com>
2019-09-09 19:29:58 +0800

    first commit

---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,49 @@
+import './transaction_item.dart';
+import 'package:flutter/material.dart';
+
+import '../models/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  final Function deleteTx;
+
+  TransactionList(this.transactions, this.deleteTx);
+
+  @override
+  Widget build(BuildContext context) {
+    return transactions.isEmpty
+        ? LayoutBuilder(
+            builder: (ctx, constraints) {
+              return Column(
+                children: <Widget>[
+                  Text(
+                    'No Transaction added yet!',
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: constraints.maxHeight * 0.6,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ],
+              );
+            },
+          )
+        : ListView(
+            children: transactions
+                .map(
+                  (tx) => TransactionItem(
+                    key: ValueKey(tx.id),
+                    transaction: tx,
+                    deleteTx: deleteTx,
+                  ),
+                )
+                .toList(),
+          );
+  }
+}
